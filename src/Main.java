7c08b4507de5238c062798b8c0d0ade661230683
Pavel <tgamb@yandex.ru>
2023-03-10 18:22:07 +0300

    добавлено меню

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            printMenu();
+            int i = scanner.nextInt();
+
+            if(i == 1) {
+                System.out.println("Выполняется команда 1!");
+            } else if(i == 2) {
+                System.out.println("Выполняется команда 2!");
+            } else if(i == 3) {
+                System.out.println("Выполняется команда 3!");
+            } else if(i == 4) {
+                System.out.println("Всего доброго!");
+                break;
+            } else {
+                System.out.println("Такой команды нет");
+            }
+        }
+    }
+
+    static void printMenu(){
+        System.out.println("Что вы хотите сделать?");
+        System.out.println("Ввести количество шагов за определённый день - 1");
+        System.out.println("Изменить цель по количеству шагов в день - 2");
+        System.out.println("Напечатать статистику за определённый месяц - 3");
+        System.out.println("Выйти из приложения - 4");
     }
 }
